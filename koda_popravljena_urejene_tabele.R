7eb6343be235f08d3ed7d1168315ed6e1bd52099
zangrjol <grjol.zan@gmail.com>
2015-04-05 00:27:16 +0200

    še za backtest izračun na koncu k nevem če je pravilen

--- koda_popravljena_urejene_tabele.R
+++ koda_popravljena_urejene_tabele.R
@@ -6,7 +6,7 @@ delnice[,1] <- NULL
 ######benchmark mora biti na zadnjem mestu pri vnašanju tickerjev!!!!
 
 #vse NA-je nadomestim z 0 (problem bo, če kje vmes NA, sepravi da ob določenem datumu ne bo podatka - 
-#UPAM DA TO NI MOŽNO!!! - DRUGAČE TREBA NAREDITI ŠE ENO ZANKO KI JE POMOJE DOST ZAJEBANA)
+#UPAM DA TO NI MOŽNO!!! - DRUGAČE TREBA NAREDITI ŠE ENO ZANKO KI JE POMOJE DOST ZAJEBANA) - V DRUGI R KODI JE NAREJENA ZANKA DELNO
 delnice[is.na(delnice)] <- 0
 
 #spremenim datum v tri stolpce z letom, mesecem in dnevom
@@ -18,6 +18,7 @@ delnice <- cbind(leto = substr(row.names(delnice), 1, 4), delnice)
 #zbriše prvo vrstico, kjer so bili prej datumi so zdaj rownames kar indeksi
 rownames(delnice) <- c(1:length(delnice[,1]))
 
+cene <- delnice
 
 #i vrstica, j stolpec - vse damo na procentualne spremembe
 
@@ -63,7 +64,8 @@ sharp <- sharp[(dnevi+1):length(sharp[,1]),] #zbrišem prvih 90 vrstic, kjer sha
 #dodajam še novo razpredelnico, ki bo povedala, v katero delnico investirati na vsakih n dni
 #vsak n-ti dan bomo pogledali za n dni nazaj, kakšno je bilo povprečje sharpovih svrednosti in izbrali najvišje
 
-n <- length(which(delnice$leto==2010))
+n <- 30
+delez <- seq(1/n,1, 1/n) #da bomo vzeli tehtano povprečje sharpov z večjo težo na čim kasnejših sharpih
 invest <- sharp
 invest[is.na(invest)] <- 0
 for (j in 4:(length(sharp[1,])-1)) { #do -1, ker zadnji stolpec je benchmark!
@@ -72,7 +74,7 @@ for (j in 4:(length(sharp[1,])-1)) { #do -1, ker zadnji stolpec je benchmark!
     k <- k+1
   }
   for (i in (k+n):length(sharp[,1])) {
-    invest[i,j] <- mean(sharp[(i-(n-1)):i,j])
+    invest[i,j] <- mean(sharp[(i-(n-1)):i,j]*delez)
   }
   for (i in 1:(k+n-1)) {invest[i,j] <- NA}
 }
@@ -92,8 +94,53 @@ colnames(trade) <- c("leto", "mesec", "dan", seq(length(trade[1,])-3, 1, -1))
 trade <- trade[,c(1,2,3,length(trade[1,]):4)] #tak vrstni red, kot hočem - 1 pomeni največji sharpov
 
 #write.csv2(trade,file="C:/zan/faks/opb/ODP/trade.csv") #tabela delnic, ki padajo po sharpovih vrednostih
+
+#lahko bi še pogledal, kakšne standardne odklone imajo sharpove in ali je trend naraščanja/padanja...
+#plot(rownames(sharp), sharp[,8], type="l")
+
+
+
+#profit, če investiramo vsako obdobje v 5 delnic z najvišjim sharpom
+proba <- (trade[-(1:3)])
+proba <- proba[-(6:length(proba[1,]))] #investiramo v 5 delnic
+profit.skupen <- 0
+for (j in 1:length(proba[1,])) {
+  for (i in 1:(length(proba[,1])-1)) {
+    profit.skupen <- profit.skupen * ((cene[dnevi+n+1+n*(i),proba[i,j]]-cene[dnevi+n+1+n*(i-1),proba[i,j]])/cene[dnevi+n+1+n*(i-1),proba[i,j]])+1
+  }
+}
+
+#profiti vsake strategije posebej (profit(i) označuje profit, če investiramo v i-to sharpovo največjo delnico)
+profit1 <- 0
+profit2 <- 0
+profit3 <- 0
+profit4 <- 0
+profit5 <- 0
+for (i in 1:(length(proba[,1])-1)) {
+  profit1 <- profit1 * ((cene[dnevi+n+1+n*(i),proba[i,1]]-cene[dnevi+n+1+n*(i-1),proba[i,1]])/cene[dnevi+n+1+n*(i-1),proba[i,1]])+1
+}
+for (i in 1:(length(proba[,1])-1)) {
+  profit2 <- profit2 * ((cene[dnevi+n+1+n*(i),proba[i,2]]-cene[dnevi+n+1+n*(i-1),proba[i,2]])/cene[dnevi+n+1+n*(i-1),proba[i,2]])+1
+}
+for (i in 1:(length(proba[,1])-1)) {
+  profit3 <- profit3 * ((cene[dnevi+n+1+n*(i),proba[i,3]]-cene[dnevi+n+1+n*(i-1),proba[i,3]])/cene[dnevi+n+1+n*(i-1),proba[i,3]])+1
+}
+for (i in 1:(length(proba[,1])-1)) {
+  profit4<- profit4 *((cene[dnevi+n+1+n*(i),proba[i,4]]-cene[dnevi+n+1+n*(i-1),proba[i,4]])/cene[dnevi+n+1+n*(i-1),proba[i,4]])+1
+}
+for (i in 1:(length(proba[,1])-1)) {
+  profit5 <- profit5 * ((cene[dnevi+n+1+n*(i),proba[i,5]]-cene[dnevi+n+1+n*(i-1),proba[i,5]])/cene[dnevi+n+1+n*(i-1),proba[i,5]])+1
+}
+
+profit.skupen-1
+profit1-1
+profit2-1
+profit3-1
+profit4-1
+profit5-1
+
+
+
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
-#lahko bi še pogledal, kakšne standardne odklone imajo sharpove in ali je trend naraščanja/padanja...
-#plot(rownames(sharp), sharp[,8], type="l")
