f0ac89a9f66b7adb7485d45a5af4e6f5ded7b385
sejpravim <jst.basket@gmail.com>
2015-05-07 14:07:58 +0200

    grjol

--- generiranje baze Postgresql.py
+++ generiranje baze Postgresql.py
@@ -8,9 +8,9 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 import auth
 
 #URL naslovi:
-URL = "https://raw.githubusercontent.com/zangrjol/Optimizacija-donosnosti-portfelja/master/proba.csv"
-#URL1 =
-#URL2 =
+URL = "https://raw.githubusercontent.com/zangrjol/Optimizacija-donosnosti-portfelja/master/tabela1_cene.csv"
+URL1 = "https://raw.githubusercontent.com/zangrjol/Optimizacija-donosnosti-portfelja/master/tabela2_spremembe.csv"
+URL2 = "https://raw.githubusercontent.com/zangrjol/Optimizacija-donosnosti-portfelja/master/tabela3_sharpe.csv"
 #URL3 =
 
 #urejanje tabel pridobljenih iz URL naslovov:
@@ -18,13 +18,13 @@ wwwcene = urllib.request.urlopen(URL)
 delnice = wwwcene.readline()
 razpredelnica = csv.reader([v.decode() for v in wwwcene], delimiter=';')
 
-##wwwsp = urllib.request.urlopen(URL1)
-##sprememba = wwwsp.readline()
-##spremembe = csv.reader([v.decode() for v in wwwsp], delimiter=';')
+wwwsp = urllib.request.urlopen(URL1)
+sprememba = wwwsp.readline()
+spremembe = csv.reader([v.decode() for v in wwwsp], delimiter=';')
 
-##wwwsharp = urllib.request.urlopen(URL2)
-##sharp = wwwsharp.readline()
-##sharpove = csv.reader([v.decode() for v in wwwsharp], delimiter=';')
+wwwsharp = urllib.request.urlopen(URL2)
+sharp = wwwsharp.readline()
+sharpove = csv.reader([v.decode() for v in wwwsharp], delimiter=';')
 ####
 ##www3 = urllib.request.urlopen(URL3)
 ##sharp5 = www3.readline()
@@ -47,6 +47,7 @@ def izbrisi():
 
 #ko ni na postgresql-u nobene tabele!! ustvarimo tabeli:
 def ustvariTabelo():
+    c = baza.cursor()
     c.execute('''CREATE TABLE delnice (
       id SERIAL PRIMARY KEY,
       simbol TEXT,
@@ -59,6 +60,7 @@ def ustvariTabelo():
     baza.commit()
 
 def ustvariImena():
+    c = baza.cursor()
     c.execute('''CREATE TABLE imena (
       id SERIAL PRIMARY KEY,
       simbol TEXT REFERENCES delnice(simbol),
@@ -71,10 +73,9 @@ def ustvariImena():
 delnice1 = delnice.decode("utf-8")
 delnice2 = delnice1.replace('\n','')
 
-####
-#sprememba1 = sprememba.decode("utf-8")
-#sprememba2 = sprememba1.replace('\n','')
-#sharp1 = sharp.decode("utf-8")
+sprememba1 = sprememba.decode("utf-8")
+sprememba2 = sprememba1.replace('\n','')
+sharp1 = sharp.decode("utf-8")
 #sharp2 = sharp.replace('\n','')
 
 #sharp5 = sharp.decode("utf-8")
@@ -87,6 +88,9 @@ delnice2 = delnice1.replace('\n','')
 #preverimo katera imena imamo in jih spravimo v seznam
 seznam = delnice2.split(sep=";")
 imena_podjetij = seznam[1:]
+imena =[]
+for i in imena_podjetij:
+    imena.append(i[1:len(i)-1])
 
 
 #funkcija, ki nam
@@ -102,8 +106,10 @@ def iskanje(datoteka):
 def podatkiCene():
     c = baza.cursor()
     for vrstica in razpredelnica:
+        spr = spremembe.__next__()
+        sha = sharpove.__next__()
         for i in range(1,len(vrstica)):
-            c.execute("INSERT INTO delnice(simbol,datum,cena) VALUES (%s,%s,%s)", [imena_podjetij[i-1],vrstica[0],float(vrstica[i].replace(',', '.'))] )
+            c.execute("INSERT INTO delnice(simbol,datum,cena,sprememba,sharp) VALUES (%s,%s,%s,%s,%s)", [imena[i-1],vrstica[0],float(vrstica[i].replace(',', '.')),float(spr[i].replace(',', '.')),float(sha[i].replace(',', '.'))] )
     c.close()
     baza.commit()
 
@@ -112,7 +118,7 @@ def podatkiSpremembe():
     c = baza.cursor()
     for vrstica in spremembe:
         for i in range(1,len(vrstica)):
-            c.execute("INSERT INTO delnice(sprememba) VALUES (%s)",[float(vrstica[i].replace(',','.'))])
+            c.execute("UPDATE delnice SET sprememba= %s",[float(vrstica[i].replace(',','.'))])
     c.close()
     baza.commit()
 
@@ -121,7 +127,7 @@ def podatkiSharp():
     c = baza.cursor()
     for vrstica in sharpove:
         for i in range(1,len(vrstica)):
-            c.execute("INSERT INTO delnice(sharp) VALUES (%s)",[float(vrstica[i].replace(',','.'))])
+            c.execute("UPDATE delnice SET sharp = %s",[float(vrstica[i].replace(',','.'))])
     c.close()
     baza.commit()
 
