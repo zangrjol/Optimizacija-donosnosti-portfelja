f377942ae81bdf0c16534c7c7e6dccaedc1b570f
sejpravim <jst.basket@gmail.com>
2015-05-07 18:28:01 +0200

    nov shiny

--- delnice/server.R
+++ delnice/server.R
@@ -10,21 +10,49 @@ shinyServer(function(input, output) {
   tabela <- tbl(conn, "delnice")
   
   output$delnice <- renderTable({
-    t <- data.frame(select(arrange(filter(tabela, simbol %in% (input$ime) & cena > input$stevilo & 
-                                            datum >= input$datum[1] & datum <= input$datum[2]),simbol),c(simbol,cena)))
+    t <- data.frame(select(arrange(filter(tabela, simbol %in% (input$select) & cena > input$stevilo & 
+                                            datum >= input$datum[1] & datum <= input$datum[2]),simbol),c(simbol,(datum),cena)))
     t
   })
+
+#   output$delnice <- renderTable({
+#     t <- data.frame(filter(tabela, simbol == input$select, datum >= input$datum[1] & datum <= input$datum[2]))
+#     t
+#   })
   
   output$delnice2 <- renderTable({
-    t <- data.frame(select(arrange(filter(tabela, simbol %in% (input$ime2) & cena > input$stevilo & 
-                                            datum >= input$datum[1] & datum <= input$datum[2]),simbol),c(simbol,cena)))
+    t <- data.frame(arrange(filter(tabela, simbol %in% (input$ime2) & cena > input$stevilo & 
+                                            datum >= input$datum[1] & datum <= input$datum[2]),simbol))
     t
   })
   
+
   output$date <- renderTable({
     t <- data.frame(select(arrange(filter(tabela, simbol %in% (input$ime2) & cena > input$stevilo & 
                                             datum >= input$datum[1] & datum <= input$datum[2]),simbol),datum))
     t$datum <- as.character(t$datum)
     t
+  })
+
+  output$sharp <- renderTable({
+    t <- data.frame(select(arrange(filter(tabela, datum == input$endatum),sharp),c(simbol,cena,sharp)))
+    t <- t[order(-t$sharp),]
+    t <- t[1:input$koliko,]
+    t
+  })
+
+  output$naslov <- renderText({
+    paste("Najboljših",input$koliko,"delnic")
+  })
+
+  output$graf <- renderPlot({
+    datumi <- data.frame(select(filter(tabela,simbol %in% (input$select)),datum))
+    cene <- data.frame(select(filter(tabela,simbol %in% (input$select)),cena))
+    cene1 <- data.frame(select(filter(tabela,simbol %in% (input$ime)),cena))
+    plot(datumi[,1],cene[,1],'l',col="red",main=paste("Gibanje delnic",input$select,"in",input$ime),xlab="èas",ylab="vrednost",
+         ylim=range(min(cene[,1],cene1[,1]),max(cene[,1],cene1[,1])))
+    par(new=TRUE)
+    plot(datumi[,1],cene1[,1],'l',ylim=range(min(cene[,1],cene1[,1]),max(cene[,1],cene1[,1])),xlab="",ylab="",col="green")
+    legend("topleft",legend=c(input$select,input$ime),col=c("red","green"),lty=c(1,1))
   })
 })
--- delnice/ui.R
+++ delnice/ui.R
@@ -1,31 +1,37 @@
 library(shiny)
-
+tabela <- read.csv2("DJIA.csv",header=TRUE,fileEncoding="Windows-1250")
+imena <- tabela$Symbol
+imena <- levels(imena)
 shinyUI(fluidPage(
   
   titlePanel("Delnice"),
   
   sidebarLayout(
     sidebarPanel(
-      textInput("ime",
-                "Simbol:",
-                value = "ci"),
-      
-      dateRangeInput("datum",label=h3("Vnesi datum"),start="2014-01-02",end="2014-12-31",language="sl"),
-      sliderInput("stevilo",label = h3("Minimalna cena"),min=10,max=150,value=10),
-      helpText("Primerjaj z"),
-      textInput("ime2",
-                "Simbol:",
-                value = "unh")   
+      selectInput("select",label="Izberi delnico",choices=imena,selected = "AAPL"),
+#       
+#       dateRangeInput("datum",label=h3("Vnesi datum"),start="2014-01-02",end="2014-12-31",language="sl"),
+#       sliderInput("stevilo",label = h3("Minimalna cena"),min=10,max=150,value=10),
+#       helpText("Primerjaj z"),
+#       textInput("ime2",
+#                 "Simbol:",
+#                 value = "AAPL"),
+      selectInput("ime", label = "Izberi delnico za primerjavo",
+                  choices = imena,selected="AXP"),
+      dateInput("endatum", label ="Izberi datum",value="2015-05-05"),
+      sliderInput("koliko",label="Koliko delnic",min=1,max=30,value=5)
     ),
     
     mainPanel(
       fluidRow(
-        column(3,
-               tableOutput("delnice")),
-        column(3,
-               tableOutput("date")),
-        column(3,
-               tableOutput("delnice2"))
+        column(3,textOutput("naslov"),
+               tableOutput("sharp")),
+        column(9,
+               plotOutput("graf"))
+#         column(3,
+#                tableOutput("date"))
+#         column(3,
+#                tableOutput("delnice2"))
       )
     )
   )
